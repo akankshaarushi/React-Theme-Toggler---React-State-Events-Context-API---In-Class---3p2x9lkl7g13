a63b814822176b032459bc1b394b60469032a656
akankshaarushi <115232645+akankshaarushi@users.noreply.github.com>
2023-03-21 19:40:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,28 @@ import '../styles/App.css';
 
 const App = () => {
 
-  const [isDarkMode, setIsDarkMode] = useState();
+const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return (
-    <div id="main">
-      <h1></h1>
-      <button></button>
-    </div>
-  )
-}
+const handleModeToggle = () => {
+setIsDarkMode(!isDarkMode);
+};
+
+const mainStyle = {
+backgroundColor: isDarkMode ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)',
+color: isDarkMode ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
+padding: '20px'
+};
+
+const h1Text = isDarkMode ? 'Dark Mode Activated' : 'Light Mode Activated';
+
+const buttonText = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+
+return (
+<div id="main" style={mainStyle}>
+<h1>{h1Text}</h1>
+<button onClick={handleModeToggle}>{buttonText}</button>
+</div>
+);
 
 
 export default App;
