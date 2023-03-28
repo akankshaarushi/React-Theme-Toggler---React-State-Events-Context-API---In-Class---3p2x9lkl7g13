962ca90152b0dcd5194636025959ec3552b1e851
Akanksha Kumari <115232645+akankshaarushi@users.noreply.github.com>
2023-03-28 16:50:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-const handleModeToggle = () => {
-setIsDarkMode(!isDarkMode);
+  return (
+    <div
+      id="main"
+      style={{
+        backgroundColor: isDarkMode ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)",
+        color: isDarkMode ? "rgb(255, 255, 255)" : "rgb(0, 0, 0)",
+      }}
+    >
+      <h1>{isDarkMode ? "Dark Mode Activated" : "Light Mode Activated"}</h1>
+      <button onClick={toggleTheme}>
+        {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+      </button>
+    </div>
+  );
 };
-
-const mainStyle = {
-backgroundColor: isDarkMode ? 'rgb(0, 0, 0)' : 'rgb(255, 255, 255)',
-color: isDarkMode ? 'rgb(255, 255, 255)' : 'rgb(0, 0, 0)',
-padding: '20px'
-};
-
-const h1Text = isDarkMode ? 'Dark Mode Activated' : 'Light Mode Activated';
-
-const buttonText = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
-
-return (
-<div id="main" style={mainStyle}>
-<h1>{h1Text}</h1>
-<button onClick={handleModeToggle}>{buttonText}</button>
-</div>
-);
-
 
 export default App;
